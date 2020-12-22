88968be22457a782e79fbccdcc653f5894a627f7
schoonboss48 <72175552+schoonboss48@users.noreply.github.com>
2020-12-21 20:48:30 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var form,player,game
+var database
+var gameState=0
+var  playerCount
+var allPlayers
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+  game=new Game()
+  game.getState()
+  game.start()
+}
+
+function draw(){
+  if(playerCount===4){
+    game.update(1)
+  }
+  if(gameState===1){
+    clear()
+    game.play()
+  }
+}
+
